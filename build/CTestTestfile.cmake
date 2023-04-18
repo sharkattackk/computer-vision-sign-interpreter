799659cc0df051e5154dd79a80cd2487e24869c7
Cameron Bennett <cameron.bennett@queensu.ca>
2023-04-17 22:50:29 -0400

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/camer/OneDrive/Desktop/CMPE 320/signInterpreter
+# Build directory: C:/Users/camer/OneDrive/Desktop/CMPE 320/signInterpreter/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
